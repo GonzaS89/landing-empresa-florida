dccb8142eb5603b86a1f847a43b535b16342f4fd
Gonza <gnzx89@gmail.com>
2024-10-31 13:50:11 -0300

    personalizando en pantalla grannde

--- src/Componentes/Infoconsultas.jsx
+++ src/Componentes/Infoconsultas.jsx
@@ -6,7 +6,7 @@ export const Infoconsultas = () => {
   return (
     <div className="consultas-bg py-12 md:px-12">
         <div className="max-w-7xl lg:my-0 lg:mx-auto flex flex-col gap-16">
-            <h1 className='text-center font-terciaria font-bold md:text-3xl lg:text-4xl xl:text-5xl text-white md:px-12'>Actualizamos nuestra sección de consultas para tu comodidad</h1>
+            <h1 className='text-center font-bold md:text-4xl lg:text-6xl text-white md:px-12'>Actualizamos nuestra sección de consultas para tu comodidad</h1>
             <div className='flex md:grid md:grid-cols-2 lg:grid-cols-3'>
             <span className="h-[550px] bg-white">
                 <img src={ConsultaImg} alt="" className='w-full h-full'/>
--- src/Componentes/Nosotros.jsx
+++ src/Componentes/Nosotros.jsx
@@ -5,9 +5,9 @@ export const Nosotros = () => {
   return (
     <div className='px-12 xl:px-0 pt-8 bg-[#F4F6FF]'>
         <div className='max-w-7xl my-o mx-auto flex flex-col md:flex-row'>
-            <div className='flex flex-col md:justify-center lg:gap-8 xl:gap-16 basis-1/2 bg-white px-12 lg:py-10 xl:py-0'>
+            <div className='flex flex-col md:justify-center md:gap-6 lg:gap-8 xl:gap-16 basis-1/2 bg-white px-12 md:py-10 xl:py-0'>
                 <h1 className='text-4xl xl:text-5xl text-left uppercase text-rojofuerte font-bold'>Quienes somos</h1>
-                <p className='md:text-sm xl:text-xl pr-16 leading-loose italic'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius impedit, iure mollitia nemo similique amet incidunt unde laborum aspernatur dignissimos eveniet alias distinctio ex consectetur quia praesentium facilis ad accusantium! Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum optio ea maiores adipisci officiis ut sit. Eius fuga distinctio temporibus cumque, dicta esse repudiandae itaque reiciendis. Repudiandae voluptatem quod illum.</p>
+                <p className='md:text-base xl:text-xl lg:pr-16 leading-loose italic'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius impedit, iure mollitia nemo similique amet incidunt unde laborum aspernatur dignissimos eveniet alias distinctio ex consectetur quia praesentium facilis ad accusantium! Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum optio ea maiores adipisci officiis ut sit. Eius fuga distinctio temporibus cumque, dicta esse repudiandae itaque reiciendis. Repudiandae voluptatem quod illum.</p>
               <button className='bg-rojofuerte hover:bg-gray-800 transition duration-300 ease-in md:py-2 xl:py-4 w-1/2 text-white font-secundaria text-xl xl:text-2xl uppercase'>Ver más</button>
             </div>
             <span className='flex basis-1/2 bg-blue-500'>
