c821be3ca2a2995ecd08577f807db6ce436a28e5
Gonza <gnzx89@gmail.com>
2024-10-31 15:58:12 -0300

    personalizando en pantalla chica

--- src/Componentes/Infoconsultas.jsx
+++ src/Componentes/Infoconsultas.jsx
@@ -8,10 +8,10 @@ export const Infoconsultas = () => {
         <div className="max-w-7xl lg:my-0 lg:mx-auto flex flex-col gap-16">
             <h1 className='text-center font-bold text-4xl md:text-5xl lg:text-6xl text-white md:px-12'>Actualizamos nuestra sección de consultas para tu comodidad</h1>
             <div className='grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3'>
-            <span className="h-[550px] bg-white max-h-[450px]">
+            <Consulta  titulo={'Horarios'} boton={'Ir a horarios'}/>
+            <span className="h-[550px] bg-white sm:max-h-[450px] md:max-h-none">
                 <img src={ConsultaImg} alt="" className='w-full h-full'/>
             </span>
-            <Consulta  titulo={'Horarios'} boton={'Ir a horarios'}/>
             <Consulta  titulo={'Abonos'} boton={'Cotizá tu abono'}/>
             </div>
         
