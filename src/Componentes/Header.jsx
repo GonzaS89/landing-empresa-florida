ec43eac8a00a41d1437bdec3e283ac3adfca2ff9
Gonza <gnzx89@gmail.com>
2024-11-07 11:19:51 -0300

    reordenando secciones

--- src/Componentes/Header.jsx
+++ src/Componentes/Header.jsx
@@ -3,6 +3,8 @@ import { IoMenu } from "react-icons/io5";
 import { IoCloseSharp } from "react-icons/io5";
 import Logo from '../assets/logo.png'
 import { Link } from 'react-scroll';
+import { Element } from "react-scroll";
+import { motion } from 'framer-motion';
 
 
 export const Header = () => {
@@ -37,14 +39,17 @@ export const Header = () => {
     return isOpen ? 'menuOpen' : 'menuClosed'
   }
 
-
   return (
-    <div className='bg-[#EEEEEE] px-8 fixed z-20 w-full'>
+
+    <Element name='header'>
+       <div className='bg-[#EEEEEE] px-8 fixed z-20 w-full'>
       <div className='h-24 my-0 mx-auto flex'>
         <div className='flex items-center justify-between w-full'>
-          <div className='flex h-[70px] lg:h-[90px]'>
+          <Link to='header' smooth={true} duration={500}>
+          <div className='flex h-[70px] lg:h-[80px] cursor-pointer'>
             <img src={Logo} alt="" className='justify-stretch' />
           </div>
+          </Link>
           <div onClick={() => setIsOpen(!isOpen)}>
             {isOpen ? <IoCloseSharp className='text-4xl icono-navbar' /> : <IoMenu className='text-4xl icono-navbar' />}
           </div>
@@ -55,11 +60,17 @@ export const Header = () => {
           </nav>
         </div>
       </div>
-      <nav className={`navbar-small bg-#EEEEEE w-full flex flex-col justify-around items-center overflow-hidden	 ${menuOpen()}`}>
+      <motion.nav 
+      initial={{height: 0}}
+      animate={{height: isOpen ? 200 : 0}}
+      duration={500}
+      className='bg-#EEEEEE w-full h-[200px] flex flex-col justify-around items-center overflow-hidden	'>
       {arraySection.map( section => (
-              <Link to={section.section} smooth={true} duration={500} offset={-96} onClick = {()=> setIsOpen(!isOpen)}className='lg:text-xl xl:text-2xl uppercase cursor-pointer'>{section.name}</Link>
+              <Link to={section.section} smooth={true} duration={500} offset={-96} onClick = {()=> setIsOpen(!isOpen)}className='text-xl uppercase cursor-pointer'>{section.name}</Link>
             ))}
-      </nav>
+      </motion.nav>
     </div>
+    </Element>
+   
   )
 }
