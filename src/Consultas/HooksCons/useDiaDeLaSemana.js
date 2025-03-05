3f292ca3dbb5ae2a89667d6b63b9e85416b87609
GonzaS89 <gnzx89@gmail.com>
2025-03-04 23:58:37 -0300

    actulizacion de horarios marzo 25

--- src/Consultas/HooksCons/useDiaDeLaSemana.js
+++ src/Consultas/HooksCons/useDiaDeLaSemana.js
@@ -6,8 +6,8 @@ export const useDiaDeLaSemana = (val) => {
 
   useEffect(() => {
     // Usamos val como clave directamente para acceder al objeto grillavacaciones
-    if (grillanormal[val]) {
-      setDiaDeLaSemana(grillanormal[val]);
+    if (grillavacaciones[val]) {
+      setDiaDeLaSemana(grillavacaciones[val]);
     }
   }, [val]);
 
