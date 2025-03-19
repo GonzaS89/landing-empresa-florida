fb65243dae75cab9d3277081e94870c8cfb065d8
Gonza <gnzx89@gmail.com>
2025-03-19 13:07:22 -0300

    ajuste en cotizacion de abonos

--- src/Consultas/Screens/CotizacionAbonos.jsx
+++ src/Consultas/Screens/CotizacionAbonos.jsx
@@ -41,7 +41,7 @@ export const CotizacionAbonos = ({
       <img src={`/img-consultas/fondoabonos.avif`} alt="" className="z-10 flex absolute h-full object-center blur-md"/>
       <div className="bg-[#0F082C] flex flex-col justify-between h-screen gap-4 z-30 opacity-85 w-full">
         <div className="flex flex-col justify-between md:gap-8 items-center h-full py-8">
-        <img src={`img-consultas/logo.png`} alt="" className="z-50 w-36"/>
+        <img src={`img-consultas/logo.avif`} alt="" className="z-50 w-36"/>
           <h1 className="text-3xl md:text-5xl text-white text-center font-londrina px-4 uppercase">El precio de tu abono es:</h1>
           <div className="text-white text-center flex flex-col gap-4">
             <CountUp
