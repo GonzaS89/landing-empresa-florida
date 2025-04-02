76cfa403f70fa4ed423070513c443ea0a7f6c221
GonzaS89 <gnzx89@gmail.com>
2025-04-02 12:40:31 -0300

    feriado 02/04

--- src/Consultas/HooksCons/useDefineFeriado.js
+++ src/Consultas/HooksCons/useDefineFeriado.js
@@ -7,6 +7,7 @@ export const useDefineFeriado = (fecha, mes) => {
     const feriados = {
       0: [1], // Enero 1
       2: [3,4,24], // Marzo 3
+      3:[2],
       11: [24, 25], // Diciembre 24 y 25
     };
 
